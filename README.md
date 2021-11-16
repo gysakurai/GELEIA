7dc35cf92ee3bb848f13f16a2e26331ebebcd205
gysakurai <94424626+gysakurai@users.noreply.github.com>
2021-11-16 00:52:45 -0300

    Adiciona arquivos

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# geleia
-Gerenciamento de Grade de Horário para Professores
+# GELEIA - Grade Escolar Livre Elaborada com Inteligência Artificial
+
+Projeto para a construção de uma aplicação web, baseada em streamlit (https://streamlit.io/), de geração automática de grade escolar (timetable) utilizando um algoritmo de meta-heurística.
---
+++ st_main.py.py
@@ -0,0 +1,37 @@
+from io import StringIO
+import streamlit as st
+import pandas as pd
+import numpy as np
+import time
+import os
+
+st.title("Gerenciamento Grade de Horário")
+
+HORARIOS = ['08:00 ~ 10:00','10:30 ~ 12:30']
+DIAS_SEMANA=['Segunda-Feira','Terça-Feira','Quarta-Feira','Quinta-Feira','Sexta-Feira']
+
+uploaded_file = st.file_uploader(
+    "Escolha o arquivo CSV", 
+    type=['csv'], 
+    help = "Insira o arquivo CSV com as informações dos professores/disciplinas/horário"
+)
+
+with st.spinner('Wait for it...'):
+    if uploaded_file is not None:
+        time.sleep(5)
+        bytes_data = uploaded_file.read()
+
+        # To read file as bytes:
+        bytes_data = uploaded_file.getvalue()
+
+        st.write(
+            """ #### Professor, Disciplina, Aulas por Semana """
+        )
+        for line in bytes_data.decode('utf-8').split('\n'):
+            st.write(line)
+
+        st.success("Done!")
+
+df = pd.DataFrame(columns=DIAS_SEMANA, index=HORARIOS)
+
+st.dataframe(df)
